3147d5c2828803249acaad47ed577f66190a8039
SolvarD <solvar@msn.com>
2021-02-05 08:45:33 +0100

    starter pack

---
+++ KinectControl/KinectControl/Manager/KinectManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace KinectControl.Manager
+{
+    public class KinectManager
+    {
+        public async Task<bool> Connect(){
+            return true;
+        }
+    }
+}
